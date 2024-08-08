9980324e5148af11881e044989d13c2272be438d
pratham-sggs <2022bit045@sggs.ac.in>
2024-08-08 20:58:36 +0530

    Calendar

--- Customizable-Calendar/printCalendar.py
+++ Customizable-Calendar/printCalendar.py
@@ -1,2 +1,11 @@
 from day import get_week_day;
 from maximumnumberofdays import maxm_days_in_month;
+
+def print_upper_box (symbol,year,layout) :
+        print(symbol*((layout*25)-5));
+        print();
+        print(symbol + " ")
+        print();
+        print(symbol*((layout*25)-5));
+
+print_upper_box("-",3000,3)
