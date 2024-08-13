704db5037fdd2d1962eca906767a1d30dbd758e1
pratham-sggs <2022bit045@sggs.ac.in>
2024-08-13 23:49:11 +0530

    Practical

--- Customizable-Calendar/printCalendar.py
+++ Customizable-Calendar/printCalendar.py
@@ -4,7 +4,7 @@ from get_number_of_rowsandcolumns import get_rows_columns_list;
 
 ongoing_month = 0;
 month_number = [];
-month = 0;
+month = 1;
 
 week_layout = "  Su Mo Tu We Th Fr Sa  "
 list_of_months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
@@ -72,34 +72,38 @@ def print_dates(symbol, year, layout):
     list_of_day = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
     
     for i in range(list_of_rc[1]):
-        maxm_days_in_month_number.append(maxm_days_in_month(list_of_months[i + month], year))
+        maxm_days_in_month_number.append(maxm_days_in_month(list_of_months[i + month-1], year))
         spaces.append(list_of_day.index(get_week_day(1, i + month, year)))
         date_of_months.append(1)
 
     while any(maxm_days_in_month_number): 
         for i in range(list_of_rc[1]):
             if maxm_days_in_month_number[i] > 0:
-                print(symbol * symbol_true + " " + " " * spaces[i], end="")
-                spaces[i] = 0
+                if i == 0 :
+                    symbol_true = True
+                print(symbol * symbol_true + "  " + "   " * spaces[i], end="")
+                
                 for k in range(7 - spaces[i]):
                     day_str = str(date_of_months[i]).rjust(2)
                     print(day_str + " ", end="")
                     
                     date_of_months[i] += 1
                 
-                    if date_of_months[i] > maxm_days_in_month_number[i]:
+                    if date_of_months[i] == maxm_days_in_month_number[i]:
                         maxm_days_in_month_number[i] = 0
                         print(" " * ((6 - spaces[i]) * 3), end="")
-                        print(symbol, end="")
+                        print(" "+ symbol, end="")
                         break
                 
-                symbol_true = False if i == 0 else symbol_true
-                print(symbol, end="")
+                symbol_true = False;
+                print(" " + symbol, end="")
+                spaces[i] = 0
             else:
+                symbol_true = False
                 print(symbol * symbol_true + " " * 24 + symbol)
         print("")
-        symbol_true = True;
-
+        symbol_true = False;
+    print(symbol + symbol*((24*list_of_rc[1]//len(symbol))+(list_of_rc[1]-1)) + symbol)
     month += 3
 
                 
@@ -122,8 +126,8 @@ def print_dates(symbol, year, layout):
 layout = "4x3"
 list_of_rc = get_rows_columns_list(layout)
 
-print_year("&",2024,"4x3")
+print_year("**",2024,"4x3")
 for i in range(0,list_of_rc[0]) :
 
-    print_month_week("&","4x3")
-    print_dates("&",2024,"4x3")
+    print_month_week("**","4x3")
+    print_dates("**",2024,"4x3")
