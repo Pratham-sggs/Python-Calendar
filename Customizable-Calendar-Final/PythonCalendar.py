acfbbed8b71e26daf5d853b2698b65aa8ba1a78f
pratham-sggs <2022bit045@sggs.ac.in>
2024-09-25 01:18:24 +0530

    Practical

---
+++ Customizable-Calendar-Final/PythonCalendar.py
@@ -0,0 +1,22 @@
+from printCalendar import *;
+from is_layout_correct import *;
+
+year = int(input("Enter The Year :"))
+if(year <= 0 or year > 9999) :
+        print("Enter Valid Year!")
+else :
+        layout = input("Enter layout :")
+        if is_layout_correct(layout) :
+                symbol = input("Enter Sepration Symbol (lenght <= 4) :")
+                if len(symbol) <= 4 :
+                        list_of_rc = get_rows_columns_list(layout)
+
+                        print_year(symbol,year,layout)
+                        for i in range(0,list_of_rc[0]) :
+
+                                print_month_week(symbol,layout)
+                                print_dates(symbol,year,layout)
+                else :
+                        print("Enter Valid Symbol")
+        else :
+                print("Enter Valid Layout")
---
+++ Customizable-Calendar-Final/printCalendar.py
@@ -0,0 +1,17 @@
+from day import get_week_day;
+from maximumnumberofdays import maxm_days_in_month;
+from get_number_of_rowsandcolumns import get_rows_columns_list;
+
+ongoing_month = 0;
+month_number = [];
+month = 1;
+
+week_layout = "  Su Mo Tu We Th Fr Sa  "
+list_of_months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+def print_year(symbol,year,layout) :
+    print()
+
+
+def print_month_week (symbol,layout) :
+    print()
